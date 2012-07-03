b70b47ee3f4d0ab5d421dbc4c7df143f1e912443
ereidland <er@evanreidlad.com>
2012-07-03 16:01:16 -0500

    Changed the phys.getTargetPoint function to only require that the lengthDelta is not 0.

--- GravityEngine/src/com/evanreidland/e/phys/phys.java
+++ GravityEngine/src/com/evanreidland/e/phys/phys.java
@@ -6,9 +6,9 @@ public class phys {
 	public static Vector3 getTargetPoint(Vector3 launcherPos, Vector3 launcherVel, Vector3 targetPos, Vector3 targetVel, double shotVel) {
 		
 		double dist = launcherPos.getDistance(targetPos);
+		double lengthDelta = shotVel - (launcherPos.plus(launcherVel).getDistance(targetPos.plus(targetVel)) - dist);
 		
-		if ( dist != 0 && shotVel != 0 ) {
-			double lengthDelta = shotVel - (launcherPos.plus(launcherVel).getDistance(targetPos.plus(targetVel)) - dist);
+		if ( lengthDelta != 0 ) {
 			return targetPos.plus(targetVel.minus(launcherVel).multipliedBy(dist/lengthDelta));
 		}
 		return targetPos.cloned();
